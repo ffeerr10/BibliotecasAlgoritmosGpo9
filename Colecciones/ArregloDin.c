a6bfe7465a28e61a892c025e836cf8576373d92d
ffeerr10 <ffeerr10@gmail.com>
2016-03-03 11:37:09 -0600

    Comentarios en funciones de arreglo dinámico

--- Colecciones/ArregloDin.c
+++ Colecciones/ArregloDin.c
@@ -8,26 +8,81 @@ ArregloDinamico arrDinInt_nuevo(){
 }
 
 void arrDinInt_ingresar(ArregloDinamico* arrDin, int valor){
+
 	arrDin->arreglo = realloc(arrDin->arreglo, (arrDin->tamanio+1)*sizeof(int) );
 	arrDin->arreglo[arrDin->tamanio] = valor;
 	arrDin->tamanio++;
+
 }
 
 void arrDinInt_imprimir(ArregloDinamico* arrDin){
-	for (int i = 0; i < arrDin->tamanio; ++i)
-	{
+
+	for (int i = 0; i < arrDin->tamanio; ++i){
 		printf("%d\n",arrDin->arreglo[i]);
 	}
+	printf("---------------\n");
+
 }
 
-/*int* arrDinInt_modificar(int * arreglo, int tamanio, int posicion){
-	return arreglo;
+void arrDinInt_remover(ArregloDinamico* arrDin, int posicion){
+
+	if(arrDin->tamanio > 1){
+		for (int i = posicion; i < arrDin->tamanio - 1 ; ++i)
+		{
+			arrDin->arreglo[i] = arrDin->arreglo[i+1];	
+		}
+	}
+
+	arrDin->arreglo = realloc(arrDin->arreglo, (arrDin->tamanio-1)*sizeof(int) );
+	arrDin->tamanio--;
+
 }
 
-int arrDinInt_remover(int * arreglo, int tamanio, int posicion){
-	return tamanio;
-}*/
+void arrDinInt_borrar(ArregloDinamico* arrDin){
 
-void arrDinInt_cerrar(ArregloDinamico* arrDin){
 	free(arrDin->arreglo);
+	arrDin->arreglo = NULL;
+	arrDin->tamanio = 0;
+
+}
+
+
+void arrDinInt_volcar(ArregloDinamico* arrDin, char *nombreArch){
+	FILE *f;
+	f = fopen(nombreArch, "wb");
+	if(f == 0) {
+		printf("Archivo %s no se pudo abrir\n", nombreArch);
+		return ;
+	}
+
+	fwrite(arrDin->arreglo,sizeof(int),arrDin->tamanio,f);
+
+	fclose(f);
+}
+
+
+int arrDinInt_cargar(ArregloDinamico* arrDin, char *nombreArch){
+
+	FILE *f;
+	f = fopen(nombreArch, "rb");
+	if(f == 0) {
+		printf("Archivo %s no se pudo abrir\n", nombreArch);
+		return 0;
+	}
+
+	arrDinInt_borrar(arrDin);
+
+	fseek(f, 0, SEEK_END);
+	int bytes = ftell(f);
+	int N = bytes/sizeof(int);
+	rewind(f);
+
+	arrDin->arreglo = (int*)malloc(bytes);
+
+	int leidos = fread(arrDin->arreglo,sizeof(int),N,f);
+
+	arrDin->tamanio = leidos;
+
+	fclose(f);
+	return leidos; 
 }
--- Colecciones/ArregloDin.h
+++ Colecciones/ArregloDin.h
@@ -4,22 +4,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Estructura arrDinInt
+	Controla un arreglo dinámico de enteros.
+		arreglo - Apuntador al arreglo dinámico
+		tamanio - Cantidad de números en la lista
+*/
 typedef struct arrDinInt {
    int *arreglo;
    int tamanio;
 } ArregloDinamico;
 
+/*Función arrDinInt_nuevo
+	Regresa un arrDinInt en ceros listo para usar.
+*/
 ArregloDinamico arrDinInt_nuevo();
 
+/*Función arrDinInt_ingresar
+	Agrega un valor al final del arreglo dinámico, aumentando la memoria correspondiente.
+*/
 void arrDinInt_ingresar(ArregloDinamico* arrDin, int valor);
 
+/*Función arrDinInt_imprimir
+	Imprime en pantalla todos los datos del arreglo dinámico
+*/
 void arrDinInt_imprimir(ArregloDinamico* arrDin);
 
-/*int* arrDinInt_modificar(int * arreglo, int tamanio, int posicion);
+/*Función arrDinInt_remover
+	Remueve un valor en una posición en especifico del arreglo, disminuyendo la memoria correspondiente.
+*/
+void arrDinInt_remover(ArregloDinamico* arrDin, int posicion);
 
-int arrDinInt_remover(int * arreglo, int tamanio, int posicion);*/
+/*Función arrDinInt_borrar
+	Libera la memoria del areglo y lo deja en ceros
+*/
+void arrDinInt_borrar(ArregloDinamico* arrDin);
 
-void arrDinInt_cerrar(ArregloDinamico* arrDin);
+/*Función arrDinInt_volcar
+	Guarda los bytes del arreglo en un archivo binario
+*/
+void arrDinInt_volcar(ArregloDinamico* arrDin, char *nombreArch);
+
+/*Función arrDinInt_cargar
+	Inicia un arreglo con los bytes de un archivo binario.
+*/
+int arrDinInt_cargar(ArregloDinamico* arrDin, char *nombreArch);
 
 
 #endif // ARREGLODIN_H_
